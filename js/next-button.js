ebc3c50e7fff1223074ecb44ca4411b4dc2cb152
Shinijuana <130153608+Shinijuana@users.noreply.github.com>
2024-09-09 12:20:18 +0200

    Update next-button.js

--- js/next-button.js
+++ js/next-button.js
@@ -51,6 +51,9 @@ AFRAME.registerComponent('next-button', {
         balloon
       } = model;
 
+      // Differenzia le animazioni per lostModel
+      const isLostModel = model === lostModel;
+
       if (!isVisible) {
         // Mostra i bottoni e nasconde i balloon al primo click
         if (textElement) {
@@ -62,13 +65,55 @@ AFRAME.registerComponent('next-button', {
             balloon.setAttribute('visible', 'false');
           }, 1000);
         }
-        [nextButton, phoneButton, mailButton, vcfButton].forEach((button) => {
-          if (button) {
-            button.setAttribute('visible', 'true');
-            button.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: 1 1 1; dir: alternate; dur: 1000');
-            button.setAttribute('animation__move', `property: position; from: 0 0 0; to: ${button.id === 'nextbutton' ? '-.8 0 .5' : button.id === 'phoneButton' ? '0 0 1.65' : button.id === 'emailButton' ? '.8 0 .5' : '0 0 -.5'}; dur: 1000; easing: linear`);
+
+        // Animazioni per arTarget
+        if (!isLostModel) {
+          if (nextButton) {
+            nextButton.setAttribute('visible', 'true');
+            nextButton.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: 1 1 1; dir: alternate; dur: 1000');
+            nextButton.setAttribute('animation__move', 'property: position; from: 0 0 0; to: -1 0 .5; dur: 1000; easing: linear');
           }
-        });
+          if (phoneButton) {
+            phoneButton.setAttribute('visible', 'true');
+            phoneButton.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: 1 1 1; dir: alternate; dur: 1000');
+            phoneButton.setAttribute('animation__move', 'property: position; from: 0 0 0; to: 0 0 2; dur: 1000; easing: linear');
+          }
+          if (mailButton) {
+            mailButton.setAttribute('visible', 'true');
+            mailButton.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: 1 1 1; dir: alternate; dur: 1000');
+            mailButton.setAttribute('animation__move', 'property: position; from: 0 0 0; to: 1 0 .5; dur: 1000; easing: linear');
+          }
+          if (vcfButton) {
+            vcfButton.setAttribute('visible', 'true');
+            vcfButton.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: 1 1 1; dir: alternate; dur: 1000');
+            vcfButton.setAttribute('animation__move', 'property: position; from: 0 0 0; to: 0 0 -.5; dur: 1000; easing: linear');
+          }
+        }
+
+        // Animazioni per lostModel
+        if (isLostModel) {
+          if (nextButton) {
+            nextButton.setAttribute('visible', 'true');
+            nextButton.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: .5 .5 .5; dir: alternate; dur: 1000');
+            nextButton.setAttribute('animation__move', 'property: position; from: 0 0 0; to: -.5 0 .8; dur: 1000; easing: linear');
+          }
+          if (phoneButton) {
+            phoneButton.setAttribute('visible', 'true');
+            phoneButton.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: .5 .5 .5; dir: alternate; dur: 1000');
+            phoneButton.setAttribute('animation__move', 'property: position; from: 0 0 0; to: 0 0 1.8; dur: 1000; easing: linear');
+          }
+          if (mailButton) {
+            mailButton.setAttribute('visible', 'true');
+            mailButton.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: .5 .5 .5; dir: alternate; dur: 1000');
+            mailButton.setAttribute('animation__move', 'property: position; from: 0 0 0; to: .5 0 .8; dur: 1000; easing: linear');
+          }
+          if (vcfButton) {
+            vcfButton.setAttribute('visible', 'true');
+            vcfButton.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: .5 .5 .5; dir: alternate; dur: 1000');
+            vcfButton.setAttribute('animation__move', 'property: position; from: 0 0 0; to: 0 0 -.8; dur: 1000; easing: linear');
+          }
+        }
+
       } else {
         // Mostra i balloon e nasconde i bottoni al secondo click
         if (textElement) {
@@ -78,146 +123,65 @@ AFRAME.registerComponent('next-button', {
           balloon.setAttribute('visible', 'true');
           balloon.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: 0.35 0.35 0.35; dir: alternate; dur: 1000');
         }
-        [nextButton, phoneButton, mailButton, vcfButton].forEach((button) => {
-          if (button) {
-            button.setAttribute('animation__pulse', 'property: scale; from: 1 1 1; to: 0 0 0; dir: alternate; dur: 1000');
-            button.setAttribute('animation__move', `property: position; from: ${button.id === 'nextbutton' ? '-.8 0 .5' : button.id === 'phoneButton' ? '0 0 1.65' : button.id === 'emailButton' ? '.8 0 .5' : '0 0 -.5'}; to: 0 0 0; dur: 1000; easing: linear`);
+
+        // Animazioni di chiusura per arTarget
+        if (!isLostModel) {
+          if (nextButton) {
+            nextButton.setAttribute('animation__pulse', 'property: scale; from: 1 1 1; to: 0 0 0; dir: alternate; dur: 1000');
+            nextButton.setAttribute('animation__move', 'property: position; from: -1 0 .5; to: 0 0 0; dur: 1000; easing: linear');
           }
-        });
+          if (phoneButton) {
+            phoneButton.setAttribute('animation__pulse', 'property: scale; from: 1 1 1; to: 0 0 0; dir: alternate; dur: 1000');
+            phoneButton.setAttribute('animation__move', 'property: position; from: 0 0 2; to: 0 0 0; dur: 1000; easing: linear');
+          }
+          if (mailButton) {
+            mailButton.setAttribute('animation__pulse', 'property: scale; from: 1 1 1; to: 0 0 0; dir: alternate; dur: 1000');
+            mailButton.setAttribute('animation__move', 'property: position; from: 1 0 .5; to: 0 0 0; dur: 1000; easing: linear');
+          }
+          if (vcfButton) {
+            vcfButton.setAttribute('animation__pulse', 'property: scale; from: 1 1 1; to: 0 0 0; dir: alternate; dur: 1000');
+            vcfButton.setAttribute('animation__move', 'property: position; from: 0 0 -.5; to: 0 0 0; dur: 1000; easing: linear');
+          }
+        }
+
+        // Animazioni di chiusura per lostModel
+        if (isLostModel) {
+          if (nextButton) {
+            nextButton.setAttribute('animation__pulse', 'property: scale; from: .5 .5 .5; to: 0 0 0; dir: alternate; dur: 1000');
+            nextButton.setAttribute('animation__move', 'property: position; from: -.5 0 .8; to: 0 0 0; dur: 1000; easing: linear');
+          }
+          if (phoneButton) {
+            phoneButton.setAttribute('animation__pulse', 'property: scale; from: .5 .5 .5; to: 0 0 0; dir: alternate; dur: 1000');
+            phoneButton.setAttribute('animation__move', 'property: position; from: 0 0 1.8; to: 0 0 0; dur: 1000; easing: linear');
+          }
+          if (mailButton) {
+            mailButton.setAttribute('animation__pulse', 'property: scale; from: .5 .5 .5; to: 0 0 0; dir: alternate; dur: 1000');
+            mailButton.setAttribute('animation__move', 'property: position; from: .5 0 .8; to: 0 0 0; dur: 1000; easing: linear');
+          }
+          if (vcfButton) {
+            vcfButton.setAttribute('animation__pulse', 'property: scale; from: .5 .5 .5; to: 0 0 0; dir: alternate; dur: 1000');
+            vcfButton.setAttribute('animation__move', 'property: position; from: 0 0 -.8; to: 0 0 0; dur: 1000; easing: linear');
+          }
+        }
 
         setTimeout(() => {
-          [nextButton, phoneButton, mailButton, vcfButton].forEach((button) => {
-            if (button) button.setAttribute('visible', 'false');
-          });
+          nextButton.setAttribute('visible', 'false');
+          phoneButton.setAttribute('visible', 'false');
+          mailButton.setAttribute('visible', 'false');
+          vcfButton.setAttribute('visible', 'false');
         }, 1000);
       }
     };
 
-    // Funzione per inizializzare gli eventi e le animazioni
-    const initializeEvents = (model) => {
-      const {
-        char,
-        nextButton,
-        phoneButton,
-        mailButton,
-        vcfButton,
-        textElement,
-        balloon,
-        closeButton,
-        finalpage,
-        refr,
-        download
-      } = model;
+    // Event listener per il click sul nextButton
+    arTarget.nextButton.addEventListener('click', () => {
+      this.isVisible = !this.isVisible;
+      synchronizeModels(this.isVisible);
+    });
 
-      // Scritta iniziale con delay
-      setTimeout(() => {
-        if (textElement) {
-          textElement.setAttribute('typewriting', 'value: Hello, I\'m Emilio Lonardo, Ceo & Co-Founder of D.O.S.. Tap for more!');
-        }
-      }, 6000);
-
-      // Funzioni bottoni
-      if (nextButton) {
-        nextButton.onclick = () => {
-          window.open('https://designopenspaces.it/', '_blank');
-        };
-      }
-
-      if (phoneButton) {
-        phoneButton.onclick = () => {
-          window.open('tel:+393274942494', '_self');
-        };
-      }
-
-      if (mailButton) {
-        mailButton.onclick = () => {
-          window.open('mailto:emilio.lonardo@designopenspaces.it', '_self');
-        };
-      }
-
-      if (vcfButton) {
-        vcfButton.onclick = () => {
-          const downloadUrl = 'https://drive.google.com/uc?export=download&id=1DqanjcQqU1FXM29gRwWMe5noW_TxAj2U';
-          window.open(downloadUrl, '_self');
-          if (textElement) {
-            textElement.setAttribute('typewriting', 'value: Downloading contact info!');
-          }
-          if (balloon) {
-            balloon.setAttribute('visible', 'true');
-            balloon.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: 0.35 0.35 0.35; dir: alternate; dur: 1000');
-            setTimeout(() => {
-              balloon.setAttribute('animation__pulse', 'property: scale; from: 0.35 0.35 0.35; to: 0 0 0; dir: alternate; dur: 1000');
-              setTimeout(() => {
-                balloon.setAttribute('visible', 'false');
-              }, 1000);
-            }, 3000);
-          }
-        };
-      }
-
-      if (closeButton) {
-        closeButton.onclick = () => {
-          const isBalloonVisible = balloon && balloon.getAttribute('visible') === 'true';
-          this.handleCloseButton(isBalloonVisible, model);
-        };
-      }
-
-      // Gestione del click su char
-      if (char) {
-        char.onclick = () => {
-          this.isVisible = !this.isVisible; // Toggle visibilità
-          synchronizeModels(this.isVisible); // Sincronizza entrambi i modelli
-        };
-      }
-    };
-
-    // Inizializza gli eventi per ar-target e lost-model
-    initializeEvents(arTarget);
-    initializeEvents(lostModel);
-  },
-
-  handleCloseButton(isBalloonVisible, model) {
-    const {
-      char,
-      textElement,
-      balloon,
-      nextButton,
-      phoneButton,
-      mailButton,
-      vcfButton,
-      finalpage,
-      refr,
-      download
-    } = model;
-
-    if (isBalloonVisible) {
-      textElement.setAttribute('typewriting', 'value: Bye Bye!');
-      if (refr) {
-        refr.onclick = () => {
-          window.location.reload();
-        };
-      }
-
-      if (download) {
-        download.onclick = () => {
-          const downloadUrl = 'https://drive.google.com/uc?export=download&id=1DqanjcQqU1FXM29gRwWMe5noW_TxAj2U';
-          window.open(downloadUrl, '_self');
-        };
-      }
-
-      // Animazioni alla chiusura
-      setTimeout(() => {
-        char.setAttribute('animation__pulse', 'property: scale; from: 1.5 1.5 1.5; to: 0 0 0; dir: alternate; dur: 1000');
-        char.setAttribute('animation__rotation', 'property: rotation; from: 90 0 0; to: 720 0 0; dir: alternate; dur: 1000');
-      }, 500);
-      setTimeout(() => {
-        if (balloon) {
-          balloon.setAttribute('visible', 'false');
-        }
-        if (finalpage) {
-          finalpage.setAttribute('visible', 'true');
-        }
-      }, 1500);
-    }
+    lostModel.nextButton.addEventListener('click', () => {
+      this.isVisible = !this.isVisible;
+      synchronizeModels(this.isVisible);
+    });
   }
 });
