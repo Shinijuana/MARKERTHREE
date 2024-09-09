fa4b1eae60ae60db6fa3f79a47c6a9dfc3c2a598
Shinijuana <130153608+Shinijuana@users.noreply.github.com>
2024-09-09 13:14:56 +0200

    Update next-button.js

--- js/next-button.js
+++ js/next-button.js
@@ -20,7 +20,7 @@ AFRAME.registerComponent('next-button', {
       char: document.querySelector('#lost-model #tracked-model'),
       nextButton: document.querySelector('#lost-model #nextbutton'),
       phoneButton: document.querySelector('#lost-model #phoneButton'),
-      mailButton: document.querySelector('#lost-model #mailButton'),
+      mailButton: document.querySelector('#lost-model #emailButton'),
       vcfButton: document.querySelector('#lost-model #vcfButton'),
       textElement: document.querySelector('#lost-model #balloon-text'),
       balloon: document.querySelector('#lost-model #balloon-plane'),
@@ -165,16 +165,131 @@ AFRAME.registerComponent('next-button', {
       }
     };
 
-    // Aggiunge l'evento di click al balloon dell'arTarget
-    arTarget.balloon.addEventListener('click', () => {
-      this.isVisible = !this.isVisible;
-      synchronizeModels(this.isVisible);
-    });
+    // Funzione per inizializzare gli eventi e le animazioni
+    const initializeEvents = (model) => {
+      const {
+        char,
+        nextButton,
+        phoneButton,
+        mailButton,
+        vcfButton,
+        textElement,
+        balloon,
+        closeButton,
+        finalpage,
+        refr,
+        download
+      } = model;
 
-    // Aggiunge l'evento di click al balloon del lostModel
-    lostModel.balloon.addEventListener('click', () => {
-      this.isVisible = !this.isVisible;
-      synchronizeModels(this.isVisible);
-    });
+      // Scritta iniziale con delay
+      setTimeout(() => {
+        if (textElement) {
+          textElement.setAttribute('typewriting', 'value: Hello, I\'m Emilio Lonardo, Ceo & Co-Founder of D.O.S.. Tap for more!');
+        }
+      }, 6000);
+
+      // Funzioni bottoni
+      if (nextButton) {
+        nextButton.onclick = () => {
+          window.open('https://designopenspaces.it/', '_blank');
+        };
+      }
+
+      if (phoneButton) {
+        phoneButton.onclick = () => {
+          window.open('tel:+393274942494', '_self');
+        };
+      }
+
+      if (mailButton) {
+        mailButton.onclick = () => {
+          window.open('mailto:emilio.lonardo@designopenspaces.it', '_self');
+        };
+      }
+
+      if (vcfButton) {
+        vcfButton.onclick = () => {
+          const downloadUrl = 'https://drive.google.com/uc?export=download&id=1DqanjcQqU1FXM29gRwWMe5noW_TxAj2U';
+          window.open(downloadUrl, '_self');
+          if (textElement) {
+            textElement.setAttribute('typewriting', 'value: Downloading contact info!');
+          }
+          if (balloon) {
+            balloon.setAttribute('visible', 'true');
+            balloon.setAttribute('animation__pulse', 'property: scale; from: 0 0 0; to: 0.35 0.35 0.35; dir: alternate; dur: 1000');
+            setTimeout(() => {
+              balloon.setAttribute('animation__pulse', 'property: scale; from: 0.35 0.35 0.35; to: 0 0 0; dir: alternate; dur: 1000');
+              setTimeout(() => {
+                balloon.setAttribute('visible', 'false');
+              }, 1000);
+            }, 3000);
+          }
+        };
+      }
+
+      if (closeButton) {
+        closeButton.onclick = () => {
+          const isBalloonVisible = balloon && balloon.getAttribute('visible') === 'true';
+          this.handleCloseButton(isBalloonVisible, model);
+        };
+      }
+
+      // Gestione del click su char
+      if (char) {
+        char.onclick = () => {
+          this.isVisible = !this.isVisible; // Toggle visibilità
+          synchronizeModels(this.isVisible); // Sincronizza entrambi i modelli
+        };
+      }
+    };
+
+    // Inizializza gli eventi per ar-target e lost-model
+    initializeEvents(arTarget);
+    initializeEvents(lostModel);
+  },
+
+  handleCloseButton(isBalloonVisible, model) {
+    const {
+      char,
+      textElement,
+      balloon,
+      nextButton,
+      phoneButton,
+      mailButton,
+      vcfButton,
+      finalpage,
+      refr,
+      download
+    } = model;
+
+    if (isBalloonVisible) {
+      textElement.setAttribute('typewriting', 'value: Bye Bye!');
+      if (refr) {
+        refr.onclick = () => {
+          window.location.reload();
+        };
+      }
+
+      if (download) {
+        download.onclick = () => {
+          const downloadUrl = 'https://drive.google.com/uc?export=download&id=1DqanjcQqU1FXM29gRwWMe5noW_TxAj2U';
+          window.open(downloadUrl, '_self');
+        };
+      }
+
+      // Animazioni alla chiusura
+      setTimeout(() => {
+        char.setAttribute('animation__pulse', 'property: scale; from: 1.5 1.5 1.5; to: 0 0 0; dir: alternate; dur: 1000');
+        char.setAttribute('animation__rotation', 'property: rotation; from: 90 0 0; to: 720 0 0; dir: alternate; dur: 1000');
+      }, 500);
+      setTimeout(() => {
+        if (balloon) {
+          balloon.setAttribute('visible', 'false');
+        }
+        if (finalpage) {
+          finalpage.setAttribute('visible', 'true');
+        }
+      }, 1500);
+    }
   }
 });
